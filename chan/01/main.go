2b3ad1fcf016eef9f958de3ee544dafb6e62ca49
Dave Augustus <davetweetlive@gmail.com>
2021-08-11 18:07:20 +0530

    Added chan

--- chan/01/main.go
+++ chan/01/main.go
@@ -2,45 +2,28 @@ package main
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
-var i int
-var wg sync.WaitGroup
-
 func main() {
-	// ch := make(chan bool)
-	count := 0
+	ch := make(chan bool)
 
-	for i := 0; i < 20; i++ {
-		fmt.Println(count)
-		wg.Add(1)
-		go func() {
-			count++
-		}()
+	for i := 0; i < 100; i++ {
+
+		go Player1(ch)
 		time.Sleep(time.Second)
-		// wg.Add(2)
-		// go Player1(ch)
-		// time.Sleep(time.Millisecond)
-		// go Player2(ch)
-		// time.Sleep(time.Millisecond)
+		go Player2(ch)
+		time.Sleep(time.Second)
 	}
 
-	wg.Wait()
 }
 
 func Player1(ch chan bool) {
-	fmt.Println("Player 1 Played it's turn")
 	ch <- true
-
-	wg.Done()
-	// fmt.Println("Player 1 Played")
+	fmt.Println("Player 1 Played")
 }
 
 func Player2(ch chan bool) {
-	fmt.Println("Player 2 Played it's turn")
 	<-ch
-
-	wg.Done()
+	fmt.Println("Player 2 Played")
 }
